8ac15f627245713d5b9ce5aef77eef49c99fe027
Quang Hung <hungnq1989@gmail.com>
2014-05-19 00:16:04 +0300

    Change to jinja2 template engine and add bootstrap3

--- src/root.py
+++ src/root.py
@@ -1,22 +1,22 @@
+# -*- coding: utf-8 -*-
 import cherrypy
-from mako.template import Template
-from mako.lookup import TemplateLookup
 from models import Page
 from tools import SAEnginePlugin
 from tools import SATool
 
-lookup = TemplateLookup(directories=['html'])
+from jinja2 import Environment, FileSystemLoader, PackageLoader, Template
+
+env = Environment(loader=FileSystemLoader(["/Users/hungnq/code/python/i38/src/templates"])) 
 
 class Root(object):
     @cherrypy.expose
     def index(self):
         # host = cherrypy.request.headers['Host']
         # return "You have successfully reached " + host
-        pages = [str(page) for page in Page.list(cherrypy.request.db)]
-        cherrypy.response.headers['content-type'] = 'text/plain'
-        return "Here are your list of messages: %s" % '\n'.join(pages)
-        # tmpl = lookup.get_template("index.html")
-        # return tmpl.render(salutation="Hello", target="World")
+        pages = [page for page in Page.list(cherrypy.request.db)]
+        cherrypy.response.headers['content-type'] = 'text/html'
+        template = env.get_template( "index.html")
+        return template.render(page_list=pages)
 
     @cherrypy.expose
     def page(self):
@@ -25,7 +25,7 @@ class Root(object):
 if __name__ == '__main__':
     SAEnginePlugin(cherrypy.engine, 'mysql+pymysql://root@127.0.0.1/bangtin').subscribe()
     cherrypy.tools.db = SATool()
-    cherrypy.tree.mount(Root(), '/', {'/': {'tools.db.on': True}})
+    cherrypy.tree.mount(Root(), '/', 'server.conf')
     cherrypy.engine.start()
     cherrypy.engine.block()
     # cherrypy.quickstart(Root(), '/', 'server.conf')
