ffa0a940d492470202e2088fbeba996f8c87dc99
Quang Hung <hungnq1989@gmail.com>
2014-05-19 14:47:28 +0300

    Re-order structure to use alembic

---
+++ src/i38/root.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import cherrypy
+from models import Page
+from tools import SAEnginePlugin
+from tools import SATool
+
+from jinja2 import Environment, FileSystemLoader, PackageLoader, Template
+
+env = Environment(loader=FileSystemLoader(["/Users/hungnq/code/python/i38/src/templates"])) 
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        pages = [page for page in Page.list(cherrypy.request.db)]
+        cherrypy.response.headers['content-type'] = 'text/html'
+        template = env.get_template( "index.html")
+        return template.render(page_list=pages)
+
+    @cherrypy.expose
+    def page(self):
+      return "This is the page content"
+
+if __name__ == '__main__':
+    SAEnginePlugin(cherrypy.engine, 'mysql+pymysql://root@127.0.0.1/bangtin').subscribe()
+    cherrypy.tools.db = SATool()
+    cherrypy.tree.mount(Root(), '/', 'server.conf')
+    cherrypy.engine.start()
+    cherrypy.engine.block()
