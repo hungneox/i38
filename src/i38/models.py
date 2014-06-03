c6deb005219a818ffb6cc9bea8af0f961e3ad0e2
Ilja Everilä <saarni@gmail.com>
2014-06-03 14:49:16 +0300

    Replace hardcoded class references
    Replaced with classmethods.

--- src/i38/models.py
+++ src/i38/models.py
@@ -67,11 +67,11 @@ class News(Base):
     def score(ups, downs):
       return ups - downs
 
-    @staticmethod
-    def _rank(ups, downs, date):
+    @classmethod
+    def _rank(cls, ups, downs, date):
       # This is reddit ranking function
       # https://github.com/iangreenleaf/reddit/blob/45e8209d8d4236367a6f7247068c13ab2307afb4/r2/r2/lib/db/_sorts.pyx#L45
-      s = News.score(ups, downs)
+      s = cls.score(ups, downs)
       order = log10(max(abs(s), 1))
       if s > 0:
           sign = 1
@@ -82,22 +82,22 @@ class News(Base):
       seconds = date - 1134028003
       return round((order * sign) * seconds / 45000, 7)
 
-    @staticmethod
-    def list(page_size, offset):
-        return cherrypy.request.db.query(News).order_by(desc(News.rank)).limit(page_size).offset(offset)
+    @classmethod
+    def list(cls, page_size, offset):
+        return cherrypy.request.db.query(cls).order_by(desc(cls.rank)).limit(page_size).offset(offset)
 
-    @staticmethod
-    def lastest(page_size, offset):
-       return cherrypy.request.db.query(News).order_by(desc(News.created_at)).limit(page_size).offset(offset)
+    @classmethod
+    def lastest(cls, page_size, offset):
+       return cherrypy.request.db.query(cls).order_by(desc(cls.created_at)).limit(page_size).offset(offset)
 
-    @staticmethod
-    def get(id):
-        return cherrypy.request.db.query(News).get(id)
+    @classmethod
+    def get(cls, id):
+        return cherrypy.request.db.query(cls).get(id)
 
-    @staticmethod
-    def count():
+    @classmethod
+    def count(cls):
       #http://stackoverflow.com/questions/14754994/why-is-sqlalchemy-count-much-slower-than-the-raw-query
-      return  cherrypy.request.db.query(func.count(News.id)).scalar()#optimize to count rows
+      return  cherrypy.request.db.query(func.count(cls.id)).scalar()#optimize to count rows
 
 class User(Base):
     __tablename__ = 'users'
@@ -120,26 +120,26 @@ class User(Base):
     def __unicode__(self):
         return self.username
 
-    @staticmethod
-    def list():
-        return cherrypy.request.db.query(User).all()
+    @classmethod
+    def list(cls):
+        return cherrypy.request.db.query(cls).all()
 
-    @staticmethod
-    def get(id):
-        return cherrypy.request.db.query(User).get(id)
+    @classmethod
+    def get(cls, id):
+        return cherrypy.request.db.query(cls).get(id)
 
-    @staticmethod
-    def check_credentials(username, password):
+    @classmethod
+    def check_credentials(cls, username, password):
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        user = cherrypy.request.db.query(User).filter_by(username=username).first()
+        user = cherrypy.request.db.query(cls).filter_by(username=username).first()
         if user is None:
             return "Username %s is not exist" % username
         if user.password != hashed_password:
             return "Incorrect password."
 
-    @staticmethod
-    def find_by_username(username):
-        user = cherrypy.request.db.query(User).filter_by(username=username).first()
+    @classmethod
+    def find_by_username(cls, username):
+        user = cherrypy.request.db.query(cls).filter_by(username=username).first()
         return user
 
 class Comment(Base):
@@ -175,13 +175,13 @@ class Comment(Base):
     def __unicode__(self):
         return self.text
 
-    @staticmethod
-    def get(id):
-        return cherrypy.request.db.query(Comment).get(id)
+    @classmethod
+    def get(cls, id):
+        return cherrypy.request.db.query(cls).get(id)
 
-    @staticmethod
-    def list(news_id):
-        return cherrypy.request.db.query(Comment).filter_by(news_id=news_id).order_by(desc(Comment.sort), Comment.path).all()
+    @classmethod
+    def list(cls, news_id):
+        return cherrypy.request.db.query(cls).filter_by(news_id=news_id).order_by(desc(cls.sort), cls.path).all()
 
     def time_ago(self):
       result = Utility.time_ago(self.created_at)
